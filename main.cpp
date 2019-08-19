5b4fdb361d63834d002c9596dd50bd87c1176b18
kevinponto <52466676+kevinponto@users.noreply.github.com>
2019-08-19 10:37:56 -0500

    added license
    added license

--- main.cpp
+++ main.cpp
@@ -4,6 +4,28 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
+//MIT License
+//
+//Copyright (c) 2019 Kevin Ponto
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+//© 2019 GitHub, Inc.
 
 using namespace cv;
 using namespace std;
@@ -53,7 +75,6 @@ struct Key
 {
     string name;
     Scalar color;
-
 };
 
 vector<Key> keys;
@@ -268,28 +289,6 @@ void DrawGrid()
     
 }
 
-void WriteColorCloud()
-{
-    Mat src = MakeSimpleImage(1, 1);
-    
-
-    {
-       
-        FILE *f = fopen ("colors.xyz", "w");
-        for( int y = 0; y < src.rows; y++ )
-        {
-            for( int x = 0; x < src.cols; x++ )
-            {
-                Vec3b v = src.at<Vec3b>(y,x);
-                
-                fprintf(f, "%d %d %d %d %d %d\n", v[0], v[1], v[2], v[0], v[1], v[2]);
-            }
-            
-        }
-        fclose(f);
-    }
-    
-}
 
 
 Mat CreateMatchedImage(int w=20, int h = 20)
@@ -332,8 +331,6 @@ Mat CreateMatchedImage(int w=20, int h = 20)
 void WriteCSV()
 {
     loadKeys();
-    
-    WriteColorCloud();
     
     //first make an image with 1 pixel per entry
     Mat src = MakeSimpleImage(1, 1);
